d2687b662391e05e9e067f70e1fa52f489f71813
AEnterprise <aenterprise2@gmail.com>
2019-10-22 22:09:15 +0200

    try to make it drive

--- Assets/WheelchairController.cs
+++ Assets/WheelchairController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Helpers;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class WheelchairController : MonoBehaviour
 {
@@ -12,8 +13,30 @@ public class WheelchairController : MonoBehaviour
         Electric
     }
 
-    public Mode controlMode = Mode.Electric;
-    public byte speedSetting;
+    public enum SpeedSetting
+    {
+        Slowest = 1,
+        Slow = 2,
+        Medium = 3,
+        Fast = 5,
+        Faster = 7
+        
+        
+    }
+    
+    //we have wheels
+    public WheelCollider frontLeftWheel;
+    public WheelCollider frontRightWheel;
+    public WheelCollider backLeftWheel;
+    public WheelCollider backRightWheel;
+    
+    //base speed modifier
+    public float baseSpeed = 10;
+    public float turnAngle = 30;
+
+    //wheelchair modes
+    public Mode controlMode;
+    public SpeedSetting speed;
 
     private TextMesh _textDisplay;
 
@@ -37,10 +60,21 @@ public class WheelchairController : MonoBehaviour
                 )
             );
         }
+        else
+        {
+            //electric controls, vroom vroom!
+            var speed = Input.GetAxis("Vertical") * baseSpeed * (int) this.speed;
+            backLeftWheel.motorTorque = speed;
+            
+            //for now just set steering heading from horizontal axes
+            //TODO: CLAMP THIS
+            var angle = Input.GetAxis("Horizontal");
+            frontRightWheel.steerAngle = frontLeftWheel.steerAngle = angle * turnAngle;
+        }
     }
 
 
-    void UpdateSpeeds(String rawSpeeds)
+    private void UpdateSpeeds(string rawSpeeds)
     {
         _textDisplay.text = rawSpeeds;
     }
